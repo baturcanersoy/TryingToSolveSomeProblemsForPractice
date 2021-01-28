65e7a9950e7b11d010b4aa07a46d7b976d3c3ef2
baturcannersoy@gmail.com <baturcannersoy@gmail.com>
2021-01-28 14:55:25 +0300

    Add project files.

---
+++ FindingSpecialNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindingSpecialNumbers//Karesinin 2 katinin 1 eksigi bir tamsayinin karesi olan sayilari bulmaya calıstık.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i < 10001; i++)
+
+            {
+                int calculate = ((i*i)*2)-1;
+                double squareRoot = Math.Sqrt(calculate);
+                string convert = squareRoot.ToString();
+                int sayi = 0;
+                if (int.TryParse(convert, out sayi))
+                {
+                    Console.WriteLine(i+"'nin karesinin 2 katinin 1 eksigi "+sayi+"'nin karesidir ve bu nedenle "+i+"özel sayidir.");
+                }
+            }
+
+        }
+    }
+}
---
+++ TryingToSolveSomeProblemsForPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryingToSolveSomeProblemsForPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
