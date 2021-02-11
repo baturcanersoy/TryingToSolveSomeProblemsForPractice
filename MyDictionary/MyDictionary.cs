932d42aed61376e9be2da9aeeaf78d00c26c219d
baturcannersoy@gmail.com <baturcannersoy@gmail.com>
2021-02-11 22:17:31 +0300

    11.02.2021

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] _keyArray;
+        Key[] _keyTempArray;
+        Value[] _valueArray;
+        Value[] _valueTempArray;
+        public MyDictionary()
+        {
+            _keyArray = new Key[0];
+            _valueArray = new Value[0];
+        }
+
+        public void Add(Key k, Value v)
+        {
+            _keyTempArray = _keyArray;
+            _valueTempArray = _valueArray;
+            _keyArray = new Key[_keyArray.Length + 1];
+            _valueArray = new Value[_valueArray.Length + 1];
+
+            for (int i = 0; i < _keyTempArray.Length; i++)
+            {
+                _keyArray[i] = _keyTempArray[i];
+                _valueArray[i] = _valueArray[i];
+            }
+
+            _keyArray[_keyArray.Length - 1] = k;
+            _valueArray[_valueArray.Length - 1] = v;
+        }
+
+       
+
+        public int Length
+        {
+            get { return _keyArray.Length; }// Bu yapı eleman sayısını verir.
+        }
+
+        public Key[] MyKey
+        {
+            get { return _keyArray; }// Bu yapı array in elemanıni getirir.
+        }
+
+        public Value[] MyValue
+        {
+            get { return _valueArray; }
+        }
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
+            //MyDictionary.Add("Mehmet", "Yıldız");
+
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Ali", "Demir");
+            Console.WriteLine(myDictionary.Length);
+            Console.WriteLine(myDictionary.MyKey[0]);
+            Console.WriteLine(myDictionary.MyValue[0]);
+            
+        }
+       
+    }
+}
+
